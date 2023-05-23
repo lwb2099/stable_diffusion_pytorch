806bf1045a29ca0eef0221cc194e2747e4a46d54
lwb2099 <liwenbo2099@outlook.com>
2023-05-23 15:33:02 +0800

    modify running env

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-'''
+"""
 @File    :   train.py
 @Time    :   2023/05/12 19:24:02
 @Author  :   Wenbo Li
 @Desc    :   Main Class for training stable diffusion model
-'''
+"""
 
 import argparse
 import math
@@ -24,36 +24,29 @@ from tqdm import tqdm
 from torchvision import transforms
 
 # build environment
-os.environ["CUDA_LAUNCH_BLOCKING"]="1"
+os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 logger = get_logger(__name__)
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Simple example of a stable diffusion training script.")
-    #TODO: Add args
-    # utils
-    parser.add_argument(
-        "--logging_dir", 
-        type=str,
-        default="logs",
-        help="log directory"
+    parser = argparse.ArgumentParser(
+        description="Simple example of a stable diffusion training script."
     )
+    # TODO: Add args
+    # utils
+    parser.add_argument("--logging_dir", type=str, default="logs", help="log directory")
     parser.add_argument(
         "--tracker",
         type=str,
         default=None,
     )
     # loading and saving
-    parser.add_argument(
-        "--tokenizer",
-        type=str,
-        default="openai/clip-vit-base-patch16"
-    )
+    parser.add_argument("--tokenizer", type=str, default="openai/clip-vit-base-patch16")
     parser.add_argument(
         "--text_encoder",
         type=str,
         default="openai/clip-vit-base-patch16",
-        help="CLIP model"
+        help="CLIP model",
     )
     parser.add_argument(
         "--dataset",
@@ -70,9 +63,14 @@ def parse_args():
         "--resume_from_checkpoint",
         type=str,
         default=None,
-        help="dir to load checkpoints from"
+        help="dir to load checkpoints from",
     )
-  
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="dir to save and load checkpoints",
+    )
+    # transform
     parser.add_argument(
         "--resolution",
         type=int,
@@ -89,11 +87,7 @@ def parse_args():
         default=True,
     )
     # training
-    parser.add_argument(
-        "--seed",
-        type=int,
-        default=0
-    )
+    parser.add_argument("--seed", type=int, default=0)
     parser.add_argument(
         "--learning_rate",
         type=float,
@@ -118,19 +112,15 @@ def parse_args():
         "--max_train_steps",
         type=int,
         default=1000,
-        help="total train steps, if provided, overrides max_train_epochs"
+        help="total train steps, if provided, overrides max_train_epochs",
     )
     parser.add_argument(
         "--max_train_epochs",
         type=int,
         default=100,
-        help="max train epochs, orverides by max_training_steps"
+        help="max train epochs, orverides by max_training_steps",
     )
-    parser.add_argument(
-        "--adam_weight_decay",
-        type=float,
-        default=0.1
-    )
+    parser.add_argument("--adam_weight_decay", type=float, default=0.1)
     # distributed training
     parser.add_argument(
         "--gradient_accumulation_steps",
@@ -142,31 +132,45 @@ def parse_args():
         action="store_true",
         default=False,
     )
-    
 
     args = parser.parse_args()
-    
+
     return args
 
 class StableDiffusionTrainer():
 
+class StableDiffusion:
     def __init__(self):
         pass
-    
-    def __build_model(self,tokenizer, text_encoder):
+
+    def __build_model(self, tokenizer, text_encoder):
         self.tokenizer = CLIPTokenizer.from_pretrained(tokenizer, use_fast=False)
         # ***************** test ******************
-        from diffusers import DDPMScheduler, UNet2DConditionModel,AutoencoderKL
-        self.noise_scheduler = DDPMScheduler.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="scheduler")  # DDPMScheduler()
-        self.unet = UNet2DConditionModel.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="unet")  # UNet2DConditonal()
+        from diffusers import DDPMScheduler, UNet2DConditionModel, AutoencoderKL
+
+        self.noise_scheduler = DDPMScheduler.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", subfolder="scheduler"
+        )  # DDPMScheduler()
+        self.unet = UNet2DConditionModel.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", subfolder="unet"
+        )  # UNet2DConditonal()
         self.text_encoder = CLIPTextModel.from_pretrained(text_encoder)
-        self.autoencoder = AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5", subfolder="vae")  # VQVAE()
+        self.autoencoder = AutoencoderKL.from_pretrained(
+            "runwayml/stable-diffusion-v1-5", subfolder="vae"
+        )  # VAE()
         # Freeze vae and text_encoder
         self.autoencoder.requires_grad_(False)
         self.text_encoder.requires_grad_(False)
-    
+        self.__save_model()
+
+    def __save_model(self):
+        self.noise_scheduler.save_pretrained("ckpt/scheduler")
+        self.unet.save_pretrained("ckpt/unet")
+        self.text_encoder.save_pretrained("ckpt/text_encoder")
+        self.autoencoder.save_pretrained("ckpt/autoencoder")
+
     def __build_optimizer(self, args):
-         # Initialize the optimizer
+        # Initialize the optimizer
         if args.use_8bit_adam:
             try:
                 import bitsandbytes as bnb
@@ -193,10 +197,12 @@ class StableDiffusionTrainer():
         if args.resume_from_checkpoint:
             ckpt_path = os.path.basename(args.resume_from_checkpoint)
         else:
-            # None, Get the most recent checkpoint
+            # None, Get the most recent checkpoint or start from scratch
             dirs = os.listdir(args.output_dir)
             dirs = [d for d in dirs if d.startswith("checkpoint")]
-            dirs = sorted(dirs, key=lambda x: int(x.split("-")[1]))  # checkpoint-100 => 100
+            dirs = sorted(
+                dirs, key=lambda x: int(x.split("-")[1])
+            )  # checkpoint-100 => 100
             ckpt_path = dirs[-1] if len(dirs) > 0 else None
         if ckpt_path is None:
             self.accelerator.print(
@@ -206,22 +212,32 @@ class StableDiffusionTrainer():
         else:
             self.accelerator.print(f"Resuming from checkpoint {ckpt_path}")
             self.accelerator.load_state(os.path.join(args.output_dir, ckpt_path))
-        #* Calculate train steps
+        # * Calculate train steps
         # total batch num / accumulate steps => actual update steps per epoch
-        num_update_steps_per_epoch = math.ceil(len(self.train_dataloader) / args.gradient_accumulation_steps)
+        num_update_steps_per_epoch = math.ceil(
+            len(self.train_dataloader) / args.gradient_accumulation_steps
+        )
         if args.max_train_steps is None:
             args.max_train_steps = args.max_train_epochs * num_update_steps_per_epoch
         else:
             # override max_train_epochs
-            args.max_train_epochs = math.ceil(args.max_train_steps / num_update_steps_per_epoch)
+            args.max_train_epochs = math.ceil(
+                args.max_train_steps / num_update_steps_per_epoch
+            )
         # actual updated steps
         self.global_step = int(ckpt_path.split("-")[1]) if ckpt_path else 0
-        self.start_epoch = self.global_step // num_update_steps_per_epoch if ckpt_path else 0
-        #* change diffusers implementation: 20 % 6 = 2 = 2*(10 % 3)
-        self.resume_step = self.global_step  % (num_update_steps_per_epoch) * args.gradient_accumulation_steps
+        self.start_epoch = (
+            self.global_step // num_update_steps_per_epoch if ckpt_path else 0
+        )
+        # * change diffusers implementation: 20 % 6 = 2 = 2*(10 % 3)
+        self.resume_step = (
+            self.global_step
+            % (num_update_steps_per_epoch)
+            * args.gradient_accumulation_steps
+        )
 
     def prepare(self, args):
-        #* 1. init accelerator
+        # * 1. init accelerator
         self.accelerator = Accelerator(
             gradient_accumulation_steps=args.gradient_accumulation_steps,
             log_with=args.tracker,
@@ -233,14 +249,14 @@ class StableDiffusionTrainer():
             level=logging.INFO,
         )
         logger.info(self.accelerator.state, main_process_only=False)
-        #* 2. set seed
+        # * 2. set seed
         if args.seed is not None:
             set_seed(args.seed)
-        #* 3. import tokenizers, models 
+        # * 3. import tokenizers, models
         self.__build_model(args.tokenizer, args.text_encoder)
-        #* 4. build optimizer
+        # * 4. build optimizer
         self.__build_optimizer(args)
-        #* 5. get dataset
+        # * 5. get dataset
         train_transforms = transforms.Compose(
         [
             transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
@@ -252,21 +268,35 @@ class StableDiffusionTrainer():
     )
         self.train_dataloader = load_dataset(args.dataset, args.data_dir, self.tokenizer, train_transforms, args.train_batch_size, self.accelerator.num_processes)
         self.lr_scheduler = get_scheduler(
-                args.lr_scheduler,
-                optimizer=self.optimizer,
-                num_warmup_steps=args.lr_warmup_steps * args.gradient_accumulation_steps,
-                num_training_steps=args.max_train_steps * args.gradient_accumulation_steps,
-            )
-        #* 5. Prepare everything with our `accelerator`.
-        self.unet, self.optimizer, self.train_dataloader, self.lr_scheduler = self.accelerator.prepare(
+            args.lr_scheduler,
+            optimizer=self.optimizer,
+            num_warmup_steps=args.lr_warmup_steps * args.gradient_accumulation_steps,
+            num_training_steps=args.max_train_steps * args.gradient_accumulation_steps,
+        )
+        # * 5. Prepare everything with our `accelerator`.
+        (
+            self.unet,
+            self.optimizer,
+            self.train_dataloader,
+            self.lr_scheduler,
+        ) = self.accelerator.prepare(
             self.unet, self.optimizer, self.train_dataloader, self.lr_scheduler
         )
-        #* 6. Move text_encode and vae to gpu and cast to weight_dtype
-        self.text_encoder.to(self.accelerator.device, dtype=self.accelerator.mixed_precision)
-        self.autoencoder.to(self.accelerator.device, dtype=self.accelerator.mixed_precision)
-        #* 7. Resume from checkpoint 
+        # * 6. Move text_encode and vae to gpu and cast to weight_dtype
+        self.weight_dtype = torch.float32
+        if self.accelerator.mixed_precision == "fp16":
+            self.weight_dtype = torch.float16
+        elif self.accelerator.mixed_precision == "bf16":
+            self.weight_dtype = torch.bfloat16
+        self.text_encoder.to(self.accelerator.device, dtype=self.weight_dtype)
+        self.autoencoder.to(self.accelerator.device, dtype=self.weight_dtype)
+        # * 7. Resume from checkpoint
         self.__resume_from_checkpoint(args)
-        total_batch_size = args.batch_size * self.accelerator.num_processes * args.gradient_accumulation_steps
+        total_batch_size = (
+            args.train_batch_size
+            * self.accelerator.num_processes
+            * args.gradient_accumulation_steps
+        )
         logger.info("**********************************************")
         logger.info(f"Total training data: {len(self.train_dataloader)}")
         logger.info(f"Total update steps: {args.max_train_steps}")
@@ -278,14 +308,14 @@ class StableDiffusionTrainer():
     def fit(self, args):
         logger.info("Start Training")
         self.progress_bar = tqdm(
-                        range(self.resume_step, args.total_train_update_steps),
-                        disable= not self.accelerator.is_main_process,
-                        desc="Step"
-                    )
-        for epoch in range(self.start_epoch, args.total_train_epochs):
+            range(self.resume_step, args.max_train_steps),
+            disable=not self.accelerator.is_main_process,
+            desc="Step",
+        )
+        for epoch in range(self.start_epoch, args.max_train_epochs):
             train_loss = 0
             for step, batch in enumerate(self.train_dataloader):
-            #* Skip steps until we reach the resumed step
+                # * Skip steps until we reach the resumed step
                 if (
                     args.resume_from_checkpoint
                     and epoch == self.first_epoch
@@ -297,13 +327,17 @@ class StableDiffusionTrainer():
                 with self.accelerator.accumulate(self.unet):
                     loss = self.__train_one_step(batch)
                 # gather loss across processes for logging
-                avg_loss = self.accelerator.gather(loss.repeat(args.train_batch_size)).mean()
-                train_loss += avg_loss.item()   
-                #* 7. backward
+                avg_loss = self.accelerator.gather(
+                    loss.repeat(args.train_batch_size)
+                ).mean()
+                train_loss += avg_loss.item()
+                # * 7. backward
                 self.accelerator.backward(loss)
                 if self.accelerator.sync_gradients:
-                    self.accelerator.clip_grad_norm_(self.unet.parameters(), args.max_grad_norm)
-                #* 8. update
+                    self.accelerator.clip_grad_norm_(
+                        self.unet.parameters(), args.max_grad_norm
+                    )
+                # * 8. update
                 self.optimizer.step()
                 self.lr_scheduler.step()
                 self.optimizer.zero_grad()
@@ -317,43 +351,52 @@ class StableDiffusionTrainer():
 
                 if self.global_step % args.checkpointing_steps == 0:
                     if self.accelerator.is_main_process:
-                        save_path = os.path.join(args.output_dir, f"checkpoint-{self.global_step}")
+                        save_path = os.path.join(
+                            args.output_dir, f"checkpoint-{self.global_step}"
+                        )
                         self.accelerator.save_state(save_path)
                         logger.info(f"Saved state to {save_path}")
 
-            logs = {"step_loss": loss.detach().item(), "lr": self.lr_scheduler.get_last_lr()[0]}
+            logs = {
+                "step_loss": loss.detach().item(),
+                "lr": self.lr_scheduler.get_last_lr()[0],
+            }
             self.progress_bar.set_postfix(**logs)
 
             if self.global_step >= args.max_train_steps:
                 break
-    
+
     def __train_one_step(self, batch):
         """
         __train_one_step: one diffusion backward step
 
         Args:
-            - batch (_type_):   
+            - batch (_type_):
                   _description_
 
         Returns:
-            - _type_:   
+            - _type_:
                   mse loss between sampled real noise and pred noise
         """
-        #* 1. encode image
-        latent_vector = self.autoencoder.encode(batch["pixel_values"]).to(self.accelerator.mixed_precision)
+        # * 1. encode image
+        latent_vector = (
+            self.autoencoder.encode(batch["pixel_values"])
+            .to(self.weight_dtype)
+            .latent_dist.sample()
+        )
         noise = torch.randn(latent_vector)
-        #* 2. Sample a random timestep for each image
+        # * 2. Sample a random timestep for each image
         bsz = latent_vector.shape[0]
         timesteps = torch.randint(0,1000, (bsz,), device=latent_vector.device)
         timesteps = timesteps.long()
-        #* 3. add noise to latent vector
+        # * 3. add noise to latent vector
         x_t = self.noise_scheduler.add_noise(latent_vector, timesteps, noise)
-        #* 4. get text encoding latent
+        # * 4. get text encoding latent
         text_condition = self.text_encoder(batch["input_ids"])[0]
         # 90 % of the time we use the true text encoding, 10 % of the time we use an epmty string
         if np.random.random() < 0.1:
-                text_condition = None
-        #* 5. predict noise
+            text_condition = None
+        # * 5. predict noise
         pred_noise = self.unet(x_t, timesteps, text_condition)
         return F.mse_loss(pred_noise.float(), noise.float(), reduction="mean")
 
